50e37f3cbb1721df3ed005715cb33cbd56d38a7c
Kağan <kagansensazli@gmail.com>
2024-02-23 16:21:49 +0300

    input mechanic added

--- Assets/Scripts/arrowController.cs
+++ Assets/Scripts/arrowController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class arrowController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _slideSpeed;
+    [SerializeField] private float _sideBounds;
+    [SerializeField] private float _lerpSpeed;
+    private Vector3 _forwardMoveAmount;
+    private inputManager _inputManager;
+
+    private void Awake()
     {
-        
+        _inputManager = GetComponent<inputManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        movePlayer();
+    }
+    
+    private void movePlayer()
+    {
+        Vector3 targetPosition = _player.transform.localPosition;
+
+        if (Input.GetMouseButton(0))
+        {
+            targetPosition.x = 0;
+            targetPosition.x = _inputManager.DragAmountX * _slideSpeed;
+            targetPosition.x = Mathf.Clamp(targetPosition.x, -_sideBounds, _sideBounds);
+
+            Vector3 targetPositionLerp = new Vector3(Mathf.Lerp(_player.localPosition.x, targetPosition.x, Time.fixedDeltaTime * _lerpSpeed),
+                Mathf.Lerp(_player.localPosition.y, targetPosition.y, Time.fixedDeltaTime * _lerpSpeed),
+                Mathf.Lerp(_player.localPosition.z, targetPosition.z, Time.fixedDeltaTime * _lerpSpeed));
+
+            _player.localPosition = targetPositionLerp;
+        }
     }
 }
