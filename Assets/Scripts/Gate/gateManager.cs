38c27e9d0d5cb8e31b29c2cca88e6a494879d370
Kağan <kagansensazli@gmail.com>
2024-02-23 17:04:20 +0300

    update gates

--- Assets/Scripts/Gate/gateManager.cs
+++ Assets/Scripts/Gate/gateManager.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+public enum gateType { Sum = 0, Minus = 1, Multiply = 2, Divide = 3 }
 public class gateManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private gateType _gateType;
+    [SerializeField] private int _gateValue;
+    [SerializeField] private TMP_Text _gateText;
+    [SerializeField] private MeshRenderer gateRend;
+    [SerializeField] private Material _blueMaterial;
+    [SerializeField] private Material _redMaterial;
+
+
+    private void Awake()
     {
-        
+        UpdateColor();
+        UpdateText();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateColor()
     {
-        
+        gateRend.material = (_gateType == gateType.Minus || _gateType == gateType.Divide) ? _redMaterial : _blueMaterial;
+    }
+    
+    private void UpdateText()
+    {
+        if (_gateType == gateType.Sum)
+            _gateText.text = $"+{_gateValue}";
+        else if (_gateType == gateType.Multiply)
+            _gateText.text = $"x{_gateValue}";
+        else if (_gateType == gateType.Minus)
+            _gateText.text = $"-{_gateValue}";
+        else
+            _gateText.text = $"÷{_gateValue}";
     }
 }
