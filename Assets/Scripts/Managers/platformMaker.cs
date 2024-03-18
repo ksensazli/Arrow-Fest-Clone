dc8f985f9ba8e7c290292e20810b71093c9a4a50
Kağan <kagansensazli@gmail.com>
2024-03-18 16:46:38 +0300

    -add action to platform maker

--- Assets/Scripts/Managers/platformMaker.cs
+++ Assets/Scripts/Managers/platformMaker.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using Dreamteck.Splines;
 using UnityEngine;
 
@@ -8,11 +10,27 @@ public class platformMaker : MonoBehaviour
     [SerializeField] private TubeGenerator _tubeGeneratorRight;
     [SerializeField] private GameObject _endPath;
 
-    private void Update()
+    private void OnEnable()
+    {
+        gameManager.onLevelLoaded += OnLevelLoaded;
+    }
+
+    private void OnDisable()
+    {
+        gameManager.onLevelLoaded -= OnLevelLoaded;
+    }
+
+    private void OnLevelLoaded()
     {
         _splineMesh.spline = levelManager.Instance.level.splineComputer;
         _tubeGeneratorLeft.spline = levelManager.Instance.level.splineComputer;
         _tubeGeneratorRight.spline = levelManager.Instance.level.splineComputer;
+        StartCoroutine(loadEndPath());
+    }
+
+    private IEnumerator loadEndPath()
+    {
+        yield return new WaitForEndOfFrame();
         _endPath.SetActive(true);
         _endPath.transform.position = _splineMesh.EvaluatePosition(1f) + 12.85f * Vector3.forward;
     }
